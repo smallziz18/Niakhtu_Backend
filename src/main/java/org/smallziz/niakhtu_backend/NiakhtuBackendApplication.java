e721893397cea02b819720bfcc6e5cbd76f7fb2c
smallziz18 <abdoulaziz17.diouf@ucad.edu.sn>
2024-09-29 17:58:48 +0000

    gestion plaignats

--- src/main/java/org/smallziz/niakhtu_backend/NiakhtuBackendApplication.java
+++ src/main/java/org/smallziz/niakhtu_backend/NiakhtuBackendApplication.java
@@ -2,12 +2,9 @@ package org.smallziz.niakhtu_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class NiakhtuBackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(NiakhtuBackendApplication.class, args);
     }
-
 }
